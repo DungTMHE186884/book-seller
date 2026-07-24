65a8709f7adab540b305f0082167b55b5fa5b110
vuminhtan24 <tanvmhe186791@fpt.edu.vn>
2026-07-24 07:47:12 +0700

    update checkout

--- frontend/lib/screens/customer/checkout_screen.dart
+++ frontend/lib/screens/customer/checkout_screen.dart
@@ -6,7 +6,7 @@ import '../../providers/auth_provider.dart';
 import '../../providers/cart_provider.dart';
 import '../../providers/order_provider.dart';
 import 'order_history_screen.dart';
- 
+           
 class CheckoutScreen extends StatefulWidget {
   const CheckoutScreen({super.key});
            
